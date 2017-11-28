9520593332329842b3d1c4bd6f8984d040980ad2
Kübra Özbaykus <kubraozbaykus@gmail.com>
2017-11-28 16:36:50 +0300

    yönlendirme yapıldı

--- app/src/main/java/com/tasarim/tasarim/Giris.java
+++ app/src/main/java/com/tasarim/tasarim/Giris.java
@@ -1,13 +1,30 @@
 package com.tasarim.tasarim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Giris extends AppCompatActivity {
+
+    TextView kayitOl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giris);
+
+        kayitOl=(TextView) findViewById(R.id.kayitOlSayfasi);
+
+        kayitOl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent =new Intent(Giris.this,KayitOl.class);
+                startActivity(intent);
+            }
+        });
     }
 }

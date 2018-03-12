71772aae2560c32457f08a07a7011204259e55e0
Kübra Özbaykus <kubraozbaykus@gmail.com>
2018-03-12 22:06:55 +0300

    Bar Chart hata düzeltmeleri yapıldı

--- app/src/main/java/com/tasarim/tasarim/AnalizSayfasi.java
+++ app/src/main/java/com/tasarim/tasarim/AnalizSayfasi.java
@@ -2,21 +2,21 @@ package com.tasarim.tasarim;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.LargeValueFormatter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -34,34 +34,23 @@ public class AnalizSayfasi extends AppCompatActivity implements SwipeRefreshLayo
     DatabaseReference dbRef;
 
     BarChart chart;
-
-    ArrayList<String> aylar=new ArrayList<String>();
-
-//    String aylar[]={"Aralık","Ağustos","Ekim","Eylül","Haziran",
-//            "Kasım","Mart","Mayıs","Nisan","Ocak","Temmuz","Şubat"};
+    float barWidth;
+    float barSpace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analiz_sayfasi);
 
-        yenileme_nesnesi = (SwipeRefreshLayout)findViewById(R.id.yenileme_nesnesi); // nesnemizi tanıttık
-        yenileme_nesnesi.setOnRefreshListener(this); // nesnenin bu Class içerinde çalışağını belirttik
-        // uygulama başlar başlamaz aktif oldu bu şekilde
+        yenileme_nesnesi = (SwipeRefreshLayout)findViewById(R.id.yenileme_nesnesi);
+        yenileme_nesnesi.setOnRefreshListener(this);
 
 
         db=FirebaseDatabase.getInstance();
         dbRef=FirebaseDatabase.getInstance().getReference();
 
-        chart=(BarChart)findViewById(R.id.barChart);
+
         pb=(ProgressBar)findViewById(R.id.progress_bar);
-
-
-        aylar.add("Ocak");aylar.add("Şubat"); aylar.add("Mart");aylar.add("Nisan");aylar.add("Mayıs");
-        aylar.add("Haziran");aylar.add("Temmuz");aylar.add("Ağustos");aylar.add("Eylül");aylar.add("Ekim");
-        aylar.add("Kasım");aylar.add("Aralık");
-
-
 
         setupPieChart();
 
@@ -75,14 +64,26 @@ public class AnalizSayfasi extends AppCompatActivity implements SwipeRefreshLayo
         startActivity(intent);
 
         Toast.makeText(AnalizSayfasi.this, "Yenileme başarılı", Toast.LENGTH_LONG).show();
-        yenileme_nesnesi.setRefreshing(false); /* nesnenin yenileme özelliği kapatıldı
-         aksi halde sürekli çalışır bu kısmı işleminiz yapılsada yapılmasada kullanın çünkü işlem başarısız olsada
-         hata mesajı verirsiniz ama işlem yapılana kadar olan kısımda bu kodu kullanmayın sonrası için kullanın */
+        yenileme_nesnesi.setRefreshing(false);
     }
 
     public void setupPieChart(){
+        barWidth = 0.7f;
+        barSpace = 0f;
 
-        final ArrayList<BarEntry> barEntries=new ArrayList<>();
+
+        chart = (BarChart)findViewById(R.id.barChart);
+        chart.setDescription(null);
+        chart.setPinchZoom(false);
+        chart.setScaleEnabled(true);
+        chart.setDrawBarShadow(false);
+        chart.setDrawGridBackground(false);
+
+        ArrayList<String> aylar=new ArrayList<String>();
+
+        aylar.add("Ocak");aylar.add("Şubat"); aylar.add("Mart");aylar.add("Nisan");aylar.add("Mayıs");
+        aylar.add("Haziran");aylar.add("Temmuz");aylar.add("Ağustos");aylar.add("Eylül");aylar.add("Ekim");
+        aylar.add("Kasım");aylar.add("Aralık");
 
         final int[] INCOME_COLORS = {
                 Color.rgb(119, 136, 153),
@@ -99,14 +100,7 @@ public class AnalizSayfasi extends AppCompatActivity implements SwipeRefreshLayo
                 Color.rgb(152 ,251, 152),
         };
 
-
-
-        chart.setNoDataText("");
-
-        Legend legend = chart.getLegend();
-        legend.setForm(Legend.LegendForm.CIRCLE);
-        legend.setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
-
+        final ArrayList barEntries=new ArrayList();
 
         final DatabaseReference dbAylar=db.getReference().child("Aylar");
 
@@ -115,24 +109,23 @@ public class AnalizSayfasi extends AppCompatActivity implements SwipeRefreshLayo
             public void onDataChange(DataSnapshot dataSnapshot) {
                 int i=0;
                 for(DataSnapshot key:dataSnapshot.getChildren()){
-                    if(!key.getValue().toString().equals("0")){
-                        barEntries.add(new BarEntry(Float.parseFloat(key.getValue().toString()),i));
-                    }
+
+                        barEntries.add(new BarEntry(i,Float.parseFloat(key.getValue().toString())));
 
                     i++;
                 }
                 pb.setVisibility(View.GONE);
 
-                chart.notifyDataSetChanged();
-                BarDataSet dataSet=new BarDataSet(barEntries,"Aylar");
+                BarDataSet set1;
+        set1 = new BarDataSet(barEntries, "Aylar");
+        set1.setColors(INCOME_COLORS);
 
-
-                BarData data=new BarData(dataSet);
-                dataSet.setColors(INCOME_COLORS);
-                chart.setData(data);
-                chart.invalidate();
-                dataSet.setValueTextSize(5);
-
+        BarData data = new BarData(set1);
+        data.setValueFormatter(new LargeValueFormatter());
+        chart.setData(data);
+        chart.getBarData().setBarWidth(barWidth);
+        chart.getData().setHighlightEnabled(false);
+        chart.invalidate();
 
             }
 
@@ -143,19 +136,22 @@ public class AnalizSayfasi extends AppCompatActivity implements SwipeRefreshLayo
         });
 
 
-        chart.setTouchEnabled(true);
-        chart.setDragEnabled(true);
-        chart.setScaleEnabled(true);
 
 
         XAxis xAxis = chart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-
+        xAxis.setGranularity(1f);
+        xAxis.setGranularityEnabled(true);
+        xAxis.setCenterAxisLabels(false);
         xAxis.setDrawGridLines(false);
-        xAxis.setDrawAxisLine(true);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(aylar));
 
-
-
+        chart.getAxisRight().setEnabled(false);
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setValueFormatter(new LargeValueFormatter());
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setSpaceTop(35f);
+        leftAxis.setAxisMinimum(0f);
 
     }
 }
